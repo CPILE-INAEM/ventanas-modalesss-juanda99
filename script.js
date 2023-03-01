f56e61ce87f45a69df3681fe5c48fdd77694da59
juanda <juandacorreo@gmail.com>
2023-03-01 14:07:57 +0100

    ESC key ok

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 console.log('Hello World!')
 
-const btnShowModal = document.querySelectorAll('.show-modal')
+// const btnShowModal = document.querySelectorAll('.show-modal')
+const btnShowModal = document.querySelectorAll('#botones')
 const modal = document.querySelector('.modal')
 const btnCloseModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
 
-console.log(btnShowModal)
-console.log(modal)
-console.log(btnCloseModal)
-console.log(overlay)
-
-const openModal = function () {
+const openModal = function (e) {
+  console.log(e, e.target)
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
@@ -24,3 +21,9 @@ btnShowModal.forEach((btn) => btn.addEventListener('click', openModal))
 
 overlay.addEventListener('click', closeModal)
 btnCloseModal.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeModal()
+  }
+})
